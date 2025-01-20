7a562a549c50c56a0ae5752d1dbd3721d693a684
Tetsuro Ando <tetsuro997@gmail.com>
2025-01-20 14:06:44 +0900

    create zero

--- go_00/ex06/main.go
+++ go_00/ex06/main.go
@@ -7,11 +7,6 @@ import (
 
 func main() {
     piscine.PrintCombN(1)
-    ft.PrintRune('\n')
     piscine.PrintCombN(3)
-    ft.PrintRune('\n')
-    piscine.PrintCombN(6)
-    ft.PrintRune('\n')
     piscine.PrintCombN(9)
-    ft.PrintRune('\n')
 }
